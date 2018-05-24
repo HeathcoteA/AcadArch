f3aa72e0645b7640285860efcde554759a49616c
HeathcoteA <amandine.heathcote@enc-sorbonne.fr>
2018-05-24 22:40:39 +0200

    création du premier script python

---
+++ catalogarch/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def accueil():
+    mots = [" Bienvenue sur la page d'accueil de catalogarch "]
+    return render_template('accueil.html', titre="Bienvenue !", mots=mots)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#retirer mode debug une fois le développement terminé !
