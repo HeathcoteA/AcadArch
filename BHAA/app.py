9b6279a552e4972687e58cc0904de8a550202653
HeathcoteA <amandine.heathcote@enc-sorbonne.fr>
2018-07-04 16:58:55 +0200

    problèmes de footer

--- BHAA/app.py
+++ BHAA/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     mots = [" Bienvenue sur la page d'accueil de catalogarch "]
-    return render_template('index.html', titre="Bienvenue !", mots=mots)
+    return render_template('pages/index.html', titre="Bienvenue !", mots=mots)
 
 if __name__ == '__main__':
     app.run(debug=True)
