1443c72b6e89a000f8f1dc03cbaaf0264ecbf085
HeathcoteA <amandine.heathcote@enc-sorbonne.fr>
2018-07-05 15:51:26 +0200

    modification de route et catalogue.html

--- BHAA/app.py
+++ BHAA/app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    mots = [" Bienvenue sur la page d'accueil de catalogarch "]
+    mots = [" Bienvenue sur la page d'accueil "]
     return render_template('pages/index.html', titre="Bienvenue !", mots=mots)
+
+@app.route('/catalogue')
+def catalogue():
+    mots = [" Bienvenue sur la page de catalogue "]
+    return render_template('pages/catalogue.html', titre="Catalogue", mots=mots)
 
 if __name__ == '__main__':
     app.run(debug=True)
